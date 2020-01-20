7bb58decff961883be4f991a1d756048314f54bc
progra <programito@gmail.com>
2020-01-20 18:48:31 +0100

    positive up

--- src/app/graficas/chart/chart.component.ts
+++ src/app/graficas/chart/chart.component.ts
@@ -69,85 +69,212 @@ export class ChartComponent implements OnInit {
         public emotioniesService: EmotioniesService
     ) {
 
+   
+        this.showLoading();
 
-        // this.chartOptions = {
+        this.chartOptions = {
 
-        //         title: {
-        //             text: this.title,
-        //             // style: {
-        //             //     color: '#E0E0E3',
-        //             //     textTransform: 'uppercase',
-        //             //     fontSize: '20px'
-        //             // }
-        //         },
+                title: {
+                    text: ''
+                },
 
 
-        //         subtitle: {
-        //             text: this.subtitle,
-        //             // style: {
-        //             //     color: '#E0E0E3',
-        //             //     textTransform: 'uppercase'
-        //             // }
-        //         },
+                subtitle: {
+                    text: ''
+                },
 
 
-        //         chart: {
-        //             polar: true,
-        //             type: 'column',
-        //             // backgroundColor: {
-        //             //     linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-        //             //     stops: [
-        //             //         [0, '#2a2a2b'],
-        //             //         [1, '#3e3e40']
-        //             //     ]
-        //             // },
-        //             // style: {
-        //             //     fontFamily: '\'Unica One\', sans-serif'
-        //             // },
-        //             // plotBorderColor: '#606063'
-        //         },
+                chart: {
+                    polar: true,
+                    type: 'column',
+                },
 
-        //         pane: {
-        //             size: '80%'
-        //         },
+                pane: {
+                    size: '80%'
+                },
 
-        //       xAxis: {
-        //         categories: this.categories,
-        //         tickmarkPlacement: 'on',
-        //         lineWidth: 0
-        //         },
+              xAxis: {
+                categories: this.categories,
+                tickmarkPlacement: 'on',
+                lineWidth: 0
+                },
 
-        //         yAxis: {
-        //             gridLineInterpolation: 'circle',
-        //             lineWidth: 0,
-        //             min: 0
-        //         },
+                yAxis: {
+                    gridLineInterpolation: 'circle',
+                    lineWidth: 0,
+                    min: 0
+                },
 
-        //       plotOptions: {
-        //         series: {
-        //           stacking: 'normal',
-        //           shadow: false,
-        //           groupPadding: 0,
-        //           pointPlacement: 'on'
-        //         }
-        //       },
+              plotOptions: {
+                series: {
+                  stacking: 'normal',
+                  shadow: false,
+                  groupPadding: 0,
+                  pointPlacement: 'on'
+                }
+              },
 
-        //       series:  this.series
+              series:  []
 
-        //       };
+              };
     }
 
     ngOnInit() {
-        this.loadChart();
+        //this.loadChart();
+        this.loadChartByStatus();
     }
 
 
     loadChart() {
+
+    
+
+        this.emotioniesService.loadAllEmotionies()
+            .subscribe((resp: any) => {
+
+                // this.position = Math.floor((Math.random() * (resp.length - 1)));
+
+                this.position=1;
+
+                this.hideLoading();
+
+              
+
+                if (resp.length > this.position) {
+                    this.partida = this.emotioniesService.savePartida(resp[this.position]);
+                    console.log(this.partida);
+
+                    console.log('emotion1', this.partida.getEmotions(0));
+
+
+                    this.series = [{
+                        data: this.partida.getEmotionsPositioned(0),
+                        name: 'Pos 1',
+                        pointPlacement: 'on'
+                    }, {
+                        data: this.partida.getEmotionsPositioned(1),
+                        name: 'Pos 2',
+                    },
+                    {
+                        data: this.partida.getEmotionsPositioned(2),
+                        name: 'Pos 3',
+                    },
+                    {
+                        data: this.partida.getEmotionsPositioned(3),
+                        name: 'Pos 4',
+                    },
+                    {
+                        data: this.partida.getEmotionsPositioned(4),
+                        name: 'Pos 5',
+                    },
+                    {
+                        data: this.partida.getEmotionsPositioned(5),
+                        name: 'Pos 6',
+                    }
+                    ];
+
+                    this.categories = [this.partida.getEmotionName(0), this.partida.getEmotionName(1), this.partida.getEmotionName(2),
+                                        this.partida.getEmotionName(3), this.partida.getEmotionName(4), this.partida.getEmotionName(5),
+                                        this.partida.getEmotionName(6), this.partida.getEmotionName(7), this.partida.getEmotionName(8),
+                                         this.partida.getEmotionName(9), this.partida.getEmotionName(10), this.partida.getEmotionName(11),
+                                         this.partida.getEmotionName(12), this.partida.getEmotionName(13), this.partida.getEmotionName(14),
+                                         this.partida.getEmotionName(15), this.partida.getEmotionName(16), this.partida.getEmotionName(17)];
+
+                    this.title = this.partida.correo;
+                    this.subtitle = this.partida.idPartida;
+                    this.chartOptions = {
+
+                        title: {
+                            text: this.title,
+                            // style: {
+                            //     color: '#E0E0E3',
+                            //     textTransform: 'uppercase',
+                            //     fontSize: '20px'
+                            // }
+                        },
+
+
+                        subtitle: {
+                            text: this.subtitle,
+                            // style: {
+                            //     color: '#E0E0E3',
+                            //     textTransform: 'uppercase'
+                            // }
+                        },
+
+
+                        chart: {
+                            polar: true,
+                            type: 'column',
+                            // backgroundColor: {
+                            //     linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                            //     stops: [
+                            //         [0, '#2a2a2b'],
+                            //         [1, '#3e3e40']
+                            //     ]
+                            // },
+                            // style: {
+                            //     fontFamily: '\'Unica One\', sans-serif'
+                            // },
+                            // plotBorderColor: '#606063'
+                        },
+
+                        pane: {
+                            size: '80%'
+                        },
+
+                        xAxis: {
+                            categories: this.categories,
+                            tickmarkPlacement: 'on',
+                            lineWidth: 0
+                        },
+
+                        yAxis: {
+                            gridLineInterpolation: 'circle',
+                            lineWidth: 0,
+                            min: 0
+                        },
+
+                        plotOptions: {
+                            series: {
+                                stacking: 'normal',
+                                shadow: false,
+                                groupPadding: 0,
+                                pointPlacement: 'on'
+                            }
+                        },
+
+                        credits:{
+                            enabled:false
+                         },
+
+                        loading: {
+                            labelStyle: {
+                                fontStyle: 'italic'
+                            }
+                        },
+
+                        series: this.series
+
+                    };
+
+
+
+                }
+
+            });
+
+
+    }
+
+    loadChart2() {
+
         this.emotioniesService.loadAllEmotionies()
             .subscribe((resp: any) => {
 
                 this.position = Math.floor((Math.random() * (resp.length - 1)));
 
+                this.hideLoading();
                 if (resp.length > this.position) {
                     this.partida = this.emotioniesService.savePartida(resp[this.position]);
                     console.log(this.partida);
@@ -244,6 +371,16 @@ export class ChartComponent implements OnInit {
                             }
                         },
 
+                        credits:{
+                            enabled:false
+                         },
+
+                        loading: {
+                            labelStyle: {
+                                fontStyle: 'italic'
+                            }
+                        },
+
                         series: this.series
 
                     };
@@ -257,6 +394,181 @@ export class ChartComponent implements OnInit {
 
     }
 
+
+    loadChartByStatus() {
+
+
+        this.emotioniesService.loadAllEmotionies()
+            .subscribe((resp: any) => {
+
+                // this.position = Math.floor((Math.random() * (resp.length - 1)));
+
+                this.position=1;
+
+                this.hideLoading();
+
+
+                if (resp.length > this.position) {
+                    this.partida = this.emotioniesService.savePartida(resp[this.position]);
+                    console.log(this.partida);
+
+                    console.log('emotion1', this.partida.getEmotions(0));
+
+
+                    // this.series = [{
+                    //     data: this.partida.getEmotionsPositioned(0),
+                    //     name: 'Pos 1',
+                    //     pointPlacement: 'on'
+                    // }, {
+                    //     data: this.partida.getEmotionsPositioned(1),
+                    //     name: 'Pos 2',
+                    // },
+                    // {
+                    //     data: this.partida.getEmotionsPositioned(2),
+                    //     name: 'Pos 3',
+                    // },
+                    // {
+                    //     data: this.partida.getEmotionsPositioned(3),
+                    //     name: 'Pos 4',
+                    // },
+                    // {
+                    //     data: this.partida.getEmotionsPositioned(4),
+                    //     name: 'Pos 5',
+                    // },
+                    // {
+                    //     data: this.partida.getEmotionsPositioned(5),
+                    //     name: 'Pos 6',
+                    // }
+                    // ];
+
+                    this.series = [{
+                        data: this.partida.getEmoPositive(true),
+                        name: 'Positivas',
+                        pointPlacement: 'on'
+                    }, {
+                        data: this.partida.getEmoPositive(false),
+                        name: 'Negativas',
+                        pointPlacement: 'on'
+                    }];
+
+                    this.categories = [this.partida.getEmotionName(0), this.partida.getEmotionName(1), this.partida.getEmotionName(2),
+                                        this.partida.getEmotionName(3), this.partida.getEmotionName(4), this.partida.getEmotionName(5),
+                                        this.partida.getEmotionName(6), this.partida.getEmotionName(7), this.partida.getEmotionName(8),
+                                         this.partida.getEmotionName(9), this.partida.getEmotionName(10), this.partida.getEmotionName(11),
+                                         this.partida.getEmotionName(12), this.partida.getEmotionName(13), this.partida.getEmotionName(14),
+                                         this.partida.getEmotionName(15), this.partida.getEmotionName(16), this.partida.getEmotionName(17)];
+
+                    this.title = this.partida.correo;
+                    this.subtitle = this.partida.idPartida;
+                    this.chartOptions = {
+
+                        title: {
+                            text: this.title,
+                            // style: {
+                            //     color: '#E0E0E3',
+                            //     textTransform: 'uppercase',
+                            //     fontSize: '20px'
+                            // }
+                        },
+
+
+                        subtitle: {
+                            text: this.subtitle,
+                            // style: {
+                            //     color: '#E0E0E3',
+                            //     textTransform: 'uppercase'
+                            // }
+                        },
+
+
+                        chart: {
+                            polar: true,
+                            type: 'column',
+                            // backgroundColor: {
+                            //     linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                            //     stops: [
+                            //         [0, '#2a2a2b'],
+                            //         [1, '#3e3e40']
+                            //     ]
+                            // },
+                            // style: {
+                            //     fontFamily: '\'Unica One\', sans-serif'
+                            // },
+                            // plotBorderColor: '#606063'
+                        },
+
+                        pane: {
+                            size: '80%'
+                        },
+
+                        xAxis: {
+                            categories: this.categories,
+                            tickmarkPlacement: 'on',
+                            lineWidth: 0
+                        },
+
+                        yAxis: {
+                            gridLineInterpolation: 'circle',
+                            lineWidth: 0,
+                            min: 0
+                        },
+
+                        plotOptions: {
+                            series: {
+                                stacking: 'normal',
+                                shadow: false,
+                                groupPadding: 0,
+                                pointPlacement: 'on'
+                            }
+                        },
+
+                        credits:{
+                            enabled:false
+                         },
+
+                        loading: {
+                            labelStyle: {
+                                fontStyle: 'italic'
+                            }
+                        },
+
+                        series: this.series
+
+                    };
+
+
+
+                }
+
+            });
+
+
+    }
+
+
+    cargar() {
+        this.loadChart();
+        this.showLoading2();
+    }
+
+
+    showLoading() {
+        const self = this;
+
+        this.chartCallback = chart => {
+            self.chart = chart;
+            self.chart.showLoading();
+        };
+    }
+
+    hideLoading() {
+        const self = this;
+        this.chart.hideLoading();
+    }
+
+    showLoading2() {
+        const self = this;
+        this.chart.showLoading();
+    }
+
 }
-
-
--- src/app/models/partida.model.ts
+++ src/app/models/partida.model.ts
@@ -22,4 +22,109 @@ export class Partida {
         }
         return emotion;
     }
+
+    getEmotionsPositioned(pos: number): number[]{
+        // 0-4 positivas
+        // 5-13 negativas
+        //14-17 positivas
+        let emotion: number[] = [];
+        let y = 0;
+        for (let j = 0; j < this.emotions.length; j++) {
+            y = j;
+            if(y>= 5 && y<=13){
+                y = y + 4;
+            } else if(y >=14){
+                y = y - 9;
+            }
+            emotion[j] = this.emotions[y][pos];
+        }
+        return emotion;
+    }
+
+    getEmotionName(pos:number): string {
+        // let nameEmotion= 'EMO';
+        if(pos>= 5 && pos<=13){
+            pos = pos + 4;
+        } else if(pos >=14){
+            pos = pos - 9;
+        }
+        pos++;
+        return 'EMO' + pos.toString();
+
+    }
+
+    getEmoTotal():number[]{
+        let emotion:number[]=[];
+        for (let j = 0; j < this.emotions.length; j++) {
+            emotion[j] = 0;
+            for ( let i=0; i<this.emotions[j].length; i++) {
+                emotion[j] = emotion[j] + this.emotions[j][i];
+            }
+        }
+        return emotion;
+    }
+
+    getEmoPositive(positivas: boolean): number[]{
+        if (positivas) {
+            return this.getEmoPositives();
+        }
+        else {
+            return this.getEmoNegatives();
+            // return [];
+        }
+ 
+    }
+
+    getEmoPositives():number[]{
+        let emotion:number[]=[];
+        let y= 0;
+
+        for(let j=0; j< 5 &&  j < this.emotions.length; j++){
+            emotion[j] = 0;
+            for ( let i=0; i<this.emotions[j].length; i++) {
+                emotion[j] = emotion[j] + this.emotions[j][i];
+            }
+        }
+
+        for(let j=5; j<14  &&  j < this.emotions.length; j++){
+            emotion[j] = 0;
+        }
+
+        for(let j=14; j < this.emotions.length; j++) {
+            y=j;
+            emotion[j] = 0;
+            y = y - 9;
+            for ( let i=0; i<this.emotions[y].length; i++) {
+                emotion[j] = emotion[j] + this.emotions[y][i];
+            }
+        }
+
+        return emotion;
+    }
+
+    
+    getEmoNegatives():number[]{
+        let emotion:number[]=[];
+        let y= 0;
+
+
+        for(let j=0; j < this.emotions.length; j++){
+            emotion[j] = 0;
+        }
+
+        for(let j=5; j < 14 && j < this.emotions.length; j++) {
+            y=j;
+            emotion[j] = 0;
+            y = y + 4;
+            for ( let i=0; i<this.emotions[y].length; i++) {
+                if(j==5){
+                    // console.log(j,y);
+                    console.log(i, this.emotions[y][i]);
+                }
+                emotion[j] = emotion[j] + this.emotions[y][i];
+            }
+        }
+
+        return emotion;
+    }
 }
