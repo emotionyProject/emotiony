35f1f93108eb0692a1b9922a65656008a75e812f
progra <programito@gmail.com>
2020-01-23 19:59:21 +0100

    dashboard components

--- src/app/pages/graficas/map/map.component.ts
+++ src/app/pages/graficas/map/map.component.ts
@@ -1,4 +1,15 @@
+
 import { Component, OnInit } from '@angular/core';
+import { EmotioniesService } from '../../../services/emotionies/emotionies.service';
+import { Partida } from '../../../models/partida.model';
+
+//import * as Highcharts from 'highcharts';
+//import MapModule from 'highcharts/modules/map';
+// const mapWorld = require('@highcharts/map-collection/custom/world.geo.json');
+
+//import * as mapWorld from '@highcharts/map-collection';
+// MapModule(Highcharts);
+
 
 @Component({
   selector: 'app-map',
@@ -7,7 +18,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  // position: number = 0;
+  // partida: Partida;
+  // title = "app";
+  // chart;
+  // updateFromInput = false;
+  // Highcharts = Highcharts;
+  // chartConstructor = "mapChart";
+  // chartCallback;
+  // chartOptions;
+  // series = [];
+
+  constructor(public emotioniesService: EmotioniesService) {
+
+    // console.log('map', mapWorld);
+
+  }
 
   ngOnInit() {
   }
--- src/app/pages/graficas/semi-circle/semi-circle.component.ts
+++ src/app/pages/graficas/semi-circle/semi-circle.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import highchartsMore from 'highcharts/highcharts-more';
+import highchartsFunnel from 'highcharts/modules/funnel';
+
+// import * as HighchartsMore from "highcharts/highcharts-more";
+import { EmotioniesService } from '../../../services/emotionies/emotionies.service';
+import { Partida } from '../../../models/partida.model';
+
+highchartsMore(Highcharts);
+highchartsFunnel(Highcharts);
 
 @Component({
   selector: 'app-semi-circle',
@@ -7,7 +17,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SemiCircleComponent implements OnInit {
 
-  constructor() { }
+  position: number = 0;
+  partida: Partida;
+  categories;
+  series;
+
+  title = 'emotions';
+  subtitle = 'subtitulo';
+  chart;
+  updateFromInput = false;
+  Highcharts = Highcharts;
+  chartConstructor = 'chart';
+
+  chartCallback;
+
+  chartOptions;
+
+  constructor(public emotioniesService: EmotioniesService) { 
+    this.chartOptions = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2017',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 60
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%'],
+            size: '110%'
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Chrome', 58.9],
+            ['Firefox', 13.29],
+            ['Internet Explorer', 13],
+            ['Edge', 3.78],
+            ['Safari', 3.42],
+            {
+                name: 'Other',
+                y: 7.61,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+};
+  }
 
   ngOnInit() {
   }
